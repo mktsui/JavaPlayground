faee58266216b0517f7c4880fcd9e512b27c9b08
mankitt <mankitt@100.102.237.74>
2016-09-27 19:02:03 +0800

    added Binary search

--- src/com/mktsui/playground/java/HelloWorld.java
+++ src/com/mktsui/playground/java/HelloWorld.java
@@ -1,10 +1,19 @@
 package com.mktsui.playground.java;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws java.io.IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		int a;
+		float b;
+		String s;
+ 
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Say something!");
+		s=in.nextLine();
 		System.out.println("Hello World again!");
 	}
 
