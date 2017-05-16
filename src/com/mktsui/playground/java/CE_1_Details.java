615def00fdd9bf8485f1a9382d165ba49075cc00
mktsui <mktsui@ymail.com>
2017-05-16 23:37:57 +0800

    CodeEval-Easy-Details: start

---
+++ src/com/mktsui/playground/java/CE_1_Details.java
@@ -0,0 +1,10 @@
+package com.mktsui.playground.java;
+
+public class CE_1_Details {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/mktsui/playground/java/QuickSort.java
@@ -0,0 +1,19 @@
+package com.mktsui.playground.java;
+
+import java.util.ArrayList;
+
+public class QuickSort {
+	// Average O(n log n)
+	
+	public static int[] quickSort(int[] sortList) {
+
+			
+		return sortList;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
