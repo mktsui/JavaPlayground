7da26882237fe6eaa5f48522a3922530783c9eb9
mktsui <mktsui@ymail.com>
2017-12-27 16:20:58 +0800

    update InsertionSort, add QuickSort

--- src/com/mktsui/playground/java/InsertionSort.java
+++ src/com/mktsui/playground/java/InsertionSort.java
@@ -2,10 +2,10 @@ package com.mktsui.playground.java;
 
 import java.util.Arrays;
 
-public class InsertionSort {
+class InsertionSort {
 	// average O(n^2), worst O(n^2)
 
-	public static int [] insertSort(int[] unsortList) {
+	static int [] insertSort(int[] unsortList) {
 		int position = 0;
 		int insertVal = 0;
 		
@@ -13,7 +13,7 @@ public class InsertionSort {
 			insertVal = unsortList[i];
 			position = i;
 			
-			while (position > 0 && unsortList[i-1] > insertVal) {
+			while (position > 0 && unsortList[position-1] > insertVal) {
 				unsortList[position] = unsortList[position-1];
 				position = position - 1;
 			}
@@ -24,11 +24,37 @@ public class InsertionSort {
 		return unsortList;
 	}
 
+	static void insertSortRecur(int[] unsortedList, int n) {
+
+		
+		if (n <=0 )
+			return;
+		
+		// put recursion here so front part of array can sort first
+		// to prevent swapping of elements (as in the while loop) later on
+		insertSortRecur(unsortedList, n-1);
+		
+		int last = unsortedList[n];
+		int j = n;
+		
+		while(j > 0 && unsortedList[j-1] > last) {
+			unsortedList[j] = unsortedList[j-1];
+			j--;
+		}
+		
+		unsortedList[j] = last; 
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] unsort_list= {14, 27, 10, 6, 75, 9};
+		
 		System.out.println("Before Sort:\n"+Arrays.toString(unsort_list)+"\n");
 		System.out.println("After Sorted:\n"+Arrays.toString(insertSort(unsort_list)));
+		/* alternative recursive method*/
+//		insertSortRecur(unsort_list, unsort_list.length-1);
+//		System.out.println("After Sorted:\n"+Arrays.toString(unsort_list));
 	}
 
 }
--- src/com/mktsui/playground/java/QuickSort.java
+++ src/com/mktsui/playground/java/QuickSort.java
@@ -1,19 +1,54 @@
 package com.mktsui.playground.java;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class QuickSort {
+class QuickSort {
 	// Average O(n log n)
 	
-	public static int[] quickSort(int[] sortList) {
-
+	static void quickSort(int[] sortList, int i, int j) {
+		if (i < j) {
 			
-		return sortList;
+		int partition = partFunc(sortList, i, j);
+		
+		
+		quickSort(sortList, i, partition-1);
+		quickSort(sortList, partition+1, j);
+		}
+	}
+	
+	static int partFunc(int[] sortList, int i, int j){
+		int privot = j;
+		int temp;
+		j--;
+		while(j>i){
+			while (sortList[i]<sortList[privot]){
+				i++;
+			}
+			while (sortList[j]>sortList[privot]){
+				j--;
+			}
+			if (j>i) {
+				temp = sortList[i];
+				sortList[i] = sortList[j];
+				sortList[j] = temp;
+			}
+		}
+		
+		temp = sortList[i];
+		sortList[i] = sortList[privot];
+		sortList[privot] = temp;
+		
+		return i;
+		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] unsortedList = {3,1,6,4,2,8,7,9,5};
+		System.out.println("Unsorted array\n" + Arrays.toString(unsortedList));
+		quickSort(unsortedList, 0, unsortedList.length-1);
+		System.out.println("Sorted array\n" + Arrays.toString(unsortedList));
 	}
 
 }
